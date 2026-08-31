2f43d45f14a729649f3b81f525a4554fcc42e56c
Leo Paggen <lpaggen@gmail.com>
2026-08-31 22:07:41 +0200

    fix symbol_type_table

--- rust/src/type_resolver/symbol_type_table.rs
+++ rust/src/type_resolver/symbol_type_table.rs
@@ -1,187 +1,164 @@
-// use crate::diagnostic::diagnostic::Diagnostic;
-// use std::collections::HashMap;
+use crate::diagnostic::diagnostic::Diagnostic;
+use crate::ir::stmt::AnnAssignIR;
+use crate::ir::stmt::StmtIR;
+use std::collections::HashMap;
 
-// use crate::diagnostic::diagnostic::DiagnosticKind;
-// use crate::diagnostic::diagnostic::Severity;
+use crate::ir::expr_ir::ConstantIR;
+use crate::ir::expr_ir::ExprIR;
+use crate::linker::global_scope_table::GlobalSymbolTable;
+use crate::linker::resolution_table::ResolutionTable;
+use crate::linker::resolved_target::ResolvedTarget;
+use crate::types::types::DimType;
+use crate::types::types::TensorTypeState;
+use crate::types::types::Type;
+use crate::{
+    linker::{symbol_ref::SymbolRef},
+};
 
-// use crate::ir::expr_ir::ConstantIR;
-// use crate::ir::expr_ir::ExprIR;
-// use crate::ir::nodes::BindingIR;
-// use crate::linker::global_scope_table::GlobalSymbolTable;
-// use crate::linker::resolution_table::ResolutionTable;
-// use crate::linker::resolved_target::ResolvedTarget;
-// use crate::types::types::CallableType;
-// use crate::types::types::ClassType;
-// use crate::types::types::DimType;
-// use crate::types::types::TensorTypeState;
-// use crate::types::types::Type;
-// use crate::{
-//     ir::nodes::DeclIR,
-//     linker::{program_table::ProgramTable, symbol_ref::SymbolRef},
-// };
+// probably won't need 'by_ref' since this struct might just be owned by the CFG pass in a later build
+pub struct SymbolTypeTable {
+    pub by_ref: HashMap<SymbolRef, Type>,
+}
 
-// pub struct SymbolTypeTable {
-//     pub by_ref: HashMap<SymbolRef, Type>,
-// }
+impl SymbolTypeTable {
+    pub fn new() -> Self {
+        Self {
+            by_ref: HashMap::new(),
+        }
+    }
 
-// impl SymbolTypeTable {
-//     pub fn new() -> Self {
-//         Self {
-//             by_ref: HashMap::new(),
-//         }
-//     }
+    pub fn get(&self, symbol_ref: &SymbolRef) -> Type {
+        self.by_ref
+            .get(symbol_ref)
+            .cloned()
+            .unwrap_or(Type::Unknown)
+    }
 
-//     pub fn get(&self, symbol_ref: &SymbolRef) -> Type {
-//         self.by_ref
-//             .get(symbol_ref)
-//             .cloned()
-//             .unwrap_or(Type::Unknown)
-//     }
+    fn parse_stmt(
+        &self,
+        program_id: i64,
+        stmt: &StmtIR,
+        symbols: &GlobalSymbolTable,
+        resolutions: &ResolutionTable,
+        diagnostics: &mut Vec<Diagnostic>,
+    ) -> Option<Type> {
+        match stmt {
+            StmtIR::Assign(assign_stmt) => {
+                let value = &assign_stmt.value;
+                let ty = match value {
+                    ExprIR::Constant(ConstantIR::IntegerLit(_)) => Type::Int,
+                    ExprIR::Constant(ConstantIR::FloatLit(_)) => Type::Float,
+                    ExprIR::Constant(ConstantIR::BooleanLit(_)) => Type::Bool,
+                    ExprIR::Constant(ConstantIR::StringLit(_)) => Type::String,
+                    ExprIR::Constant(ConstantIR::NoneLit(_)) => Type::None,
+                    ExprIR::Constant(ConstantIR::EllipsisLit(_)) => Type::Ellipsis,
+                    ExprIR::Constant(ConstantIR::BytesLit(_)) => Type::Bytes,
+                    ExprIR::Constant(ConstantIR::ComplexLit(_)) => Type::Complex,
+                    _ => Type::Unknown, // what we cannot resolve directly gets an Unknown type, we will resolve it later.
+                };
 
-//     fn parse_binding(
-//         &self,
-//         program_id: i64,
-//         binding: &BindingIR,
-//         symbols: &GlobalSymbolTable,
-//         resolutions: &ResolutionTable,
-//         diagnostics: &mut Vec<Diagnostic>,
-//     ) -> Type {
-//         match &binding.kind {
-//             BindingKind::Assign => {
-//                 let value = match &binding.value {
-//                     Some(value) => &**value,
-//                     None => {
-//                         diagnostics.push(Diagnostic {
-//                             severity: Severity::ERROR,
-//                             span: binding.span.clone(),
-//                             kind: DiagnosticKind::MissingBindingValue,
-//                             message: "frontend found no binding value".into(),
-//                         });
-//                         return Type::Unknown;
-//                     }
-//                 };
-//                 match value {
-//                     ExprIR::Constant(ConstantIR::IntegerLit(_)) => Type::Int,
-//                     ExprIR::Constant(ConstantIR::FloatLit(_)) => Type::Float,
-//                     ExprIR::Constant(ConstantIR::BooleanLit(_)) => Type::Bool,
-//                     ExprIR::Constant(ConstantIR::StringLit(_)) => Type::String,
-//                     ExprIR::Constant(ConstantIR::NoneLit(_)) => Type::None,
-//                     ExprIR::Constant(ConstantIR::EllipsisLit(_)) => Type::Ellipsis,
-//                     ExprIR::Constant(ConstantIR::BytesLit(_)) => Type::Bytes,
-//                     ExprIR::Constant(ConstantIR::ComplexLit(_)) => Type::Complex,
-//                     _ => Type::Unknown, // what we cannot resolve directly gets an Unknown type, we will resolve it later.
-//                 }
-//             }
+                Some(ty)
+            }
 
-//             BindingKind::AnnAssign => {
-//                 self.parse_annotation(program_id, &binding, symbols, resolutions, diagnostics)
-//             }
+            StmtIR::AnnAssign(annassign_stmt) => {
+                Some(self.parse_annotation(
+                    program_id,
+                    annassign_stmt,
+                    symbols,
+                    resolutions,
+                    diagnostics,
+                ))
+            }
 
-//             BindingKind::Unknown => {
-//                 // safe to deprecate ? makes no sense to return Unknown at any point after parsing
-//                 diagnostics.push(Diagnostic {
-//                     severity: Severity::ERROR,
-//                     span: binding.span.clone(),
-//                     kind: DiagnosticKind::InvalidBindingIR,
-//                     message: "binding kind was not classified by the frontend".into(),
-//                 });
-//                 Type::Unknown
-//             }
-//         }
-//     }
+            // this doesn't make sense, should just be assign and annassign ..? what else could be Unknown, nothing
+            _ => {
+                None
+            }
+        }
+    }
 
-//     fn parse_annotation(
-//         &self,
-//         program_id: i64,
-//         binding: &BindingIR,
-//         symbols: &GlobalSymbolTable,
-//         resolutions: &ResolutionTable,
-//         diagnostics: &mut Vec<Diagnostic>,
-//     ) -> Type {
-//         // only bindings with annotations arrive here
-//         let annotation = match &binding.annotation {
-//             Some(annotation) => annotation,
-//             None => {
-//                 diagnostics.push(Diagnostic {
-//                     severity: Severity::ERROR,
-//                     span: binding.span.clone(),
-//                     kind: DiagnosticKind::InvalidBindingIR,
-//                     message: "annotated binding has no annotation".into(),
-//                 });
-//                 return Type::Unknown;
-//             }
-//         };
+    fn parse_annotation(
+        &self,
+        program_id: i64,
+        annassign_stmt: &AnnAssignIR,
+        symbols: &GlobalSymbolTable,
+        resolutions: &ResolutionTable,
+        diagnostics: &mut Vec<Diagnostic>,
+    ) -> Type {
+        // only bindings with annotations arrive here
+        let annotation = &annassign_stmt.annotation;
 
-//         let root: Type = match annotation.head.root.as_str() {
-//             "int" => Type::Int,
-//             "float" => Type::Float,
-//             "bool" => Type::Bool,
-//             "str" => Type::String,
-//             "None" => Type::None,
-//             _ => self.resolve_annotation_path(
-//                 // check global symbol table to get the ref, ie is this torch, numpy, Local, true Unknown?
-//                 annotation.head.root.as_str(),
-//                 annotation.head.attrs.as_slice(),
-//                 program_id,
-//                 symbols,
-//                 resolutions,
-//             ),
-//         };
+        let root: Type = match annotation.head.root.as_str() {
+            "int" => Type::Int,
+            "float" => Type::Float,
+            "bool" => Type::Bool,
+            "str" => Type::String,
+            "None" => Type::None,
+            _ => self.resolve_annotation_path(
+                // check global symbol table to get the ref, ie is this torch, numpy, Local, true Unknown?
+                annotation.head.root.as_str(),
+                annotation.head.attrs.as_slice(),
+                program_id,
+                symbols,
+                resolutions,
+            ),
+        };
 
-//         root // safe? or allow Unknown..? 
-//     }
+        root
+    }
 
-//     fn resolve_annotation_path(
-//         &self,
-//         root: &str,
-//         attrs: &[String],
-//         program_id: i64,
-//         symbols: &GlobalSymbolTable,
-//         resolutions: &ResolutionTable,
-//     ) -> Type {
-//         let symbol_ref = match symbols.lookup(program_id, root) {
-//             Some(symbol_ref) => symbol_ref,
-//             None => return Type::Unknown,
-//         };
+    fn resolve_annotation_path(
+        &self,
+        root: &str,
+        attrs: &[String],
+        program_id: i64,
+        symbols: &GlobalSymbolTable,
+        resolutions: &ResolutionTable,
+    ) -> Type {
+        let symbol_ref = match symbols.lookup(program_id, root) {
+            Some(symbol_ref) => symbol_ref,
+            None => return Type::Unknown,
+        };
 
-//         let target = match resolutions.imports.get(symbol_ref) {
-//             Some(target) => target,
-//             None => return Type::Unknown,
-//         };
+        let target = match resolutions.imports.get(symbol_ref) {
+            Some(target) => target,
+            None => return Type::Unknown,
+        };
 
-//         match target {
-//             ResolvedTarget::Local(local_ref) if attrs.is_empty() => {
-//                 self.by_ref.get(local_ref).cloned().unwrap_or(Type::Unknown)
-//             }
+        match target {
+            ResolvedTarget::Local(local_ref) if attrs.is_empty() => {
+                self.by_ref.get(local_ref).cloned().unwrap_or(Type::Unknown)
+            }
 
-//             ResolvedTarget::External { module, name } => {
-//                 self.resolve_external_path(module, name, attrs)
-//             }
+            ResolvedTarget::External { module, name } => {
+                self.resolve_external_path(module, name, attrs)
+            }
 
-//             _ => Type::Unknown,
-//         }
-//     }
+            _ => Type::Unknown,
+        }
+    }
 
-//     fn resolve_external_path(&self, module: &str, imported_name: &str, attrs: &[String]) -> Type {
-//         let mut path = Vec::new();
+    fn resolve_external_path(&self, module: &str, imported_name: &str, attrs: &[String]) -> Type {
+        let mut path = Vec::new();
 
-//         // `import torch`: the imported name represents the module itself.
-//         if imported_name != module {
-//             path.push(imported_name);
-//         }
+        // `import torch`: the imported name represents the module itself.
+        if imported_name != module {
+            path.push(imported_name);
+        }
 
-//         path.extend(attrs.iter().map(String::as_str));
+        path.extend(attrs.iter().map(String::as_str));
 
-//         match (module, path.as_slice()) {
-//             ("torch", ["Tensor"]) => Type::Tensor(TensorTypeState::Unresolved),
+        match (module, path.as_slice()) {
+            ("torch", ["Tensor"]) => Type::Tensor(TensorTypeState::Unresolved),
 
-//             ("torch", ["Size"]) => Type::Dim(DimType::Unknown),
+            ("torch", ["Size"]) => Type::Dim(DimType::Unknown),
 
-//             ("torch", ["nn", "Parameter"]) => Type::Tensor(TensorTypeState::Unresolved),
+            ("torch", ["nn", "Parameter"]) => Type::Tensor(TensorTypeState::Unresolved),
 
-//             _ => Type::Unknown,
-//         }
-//     }
+            _ => Type::Unknown,
+        }
+    }
+}
 
 //     pub fn build(
 //         &mut self,
